58efb23d9d89df09f577c66843024d2e90500cb5
gayathribheemesh <gayathrib@in.ibm.com>
2024-09-20 19:50:59 +0530

    test-1

--- solutions/e2e/variables.tf
+++ solutions/e2e/variables.tf
@@ -1,7 +1,5 @@
 #####################################################
-#
 # Required Parameters
-#
 #####################################################
 variable "ibmcloud_api_key" {
   description = "The IBM Cloud platform API key needed to deploy IAM enabled resources."
