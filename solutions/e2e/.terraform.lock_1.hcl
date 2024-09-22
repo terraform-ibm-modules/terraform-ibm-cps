1fcba5651d88530a19931568569346b75ae9a38b
gayathribheemesh <gayathrib@in.ibm.com>
2024-09-22 12:44:55 +0530

    test

---
+++ solutions/e2e/.terraform.lock_1.hcl
@@ -0,0 +1,16 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/ibm-cloud/ibm" {
+  version     = "1.68.1"
+  constraints = "1.68.1"
+  hashes = [
+    "h1:M+lLYGgnMhatpA9ITdXlmq5Ekp+OneZhLVmMZbmu+WY=",
+    "zh:6ab9ed85d5644eb4a8a56c5f26b1b85a6db7690076860c8f896da432349083c7",
+    "zh:89be33e8fa52cdc58a4dbec5e1cb20c0c8c54a577e4259fca8af24245b3b8c17",
+    "zh:afac64a658d2652ab387d4d666e94e0efcae37a513d8d77dc46b98dbf97ec91f",
+    "zh:baa6b706e831711457517f05de84b8013dba8de55034fede73922ac4511554e7",
+    "zh:d024c181462b31d7053d74fdf38cf531eedbe98fda09c6c32496f047a2097b5e",
+    "zh:f0bfd3b133326f5f2d96d8f4ed6593279cf6ea5662717f7b15f33eebbc76125b",
+  ]
+}
---
+++ solutions/e2e/main.tf
@@ -0,0 +1,67 @@
+locals {
+  cos_service_credentials  = jsondecode(var.ibmcloud_cos_service_credentials)
+  cos_apikey               = local.cos_service_credentials.apikey
+  cos_resource_instance_id = local.cos_service_credentials.resource_instance_id
+
+
+
+  ibmcloud_cos_cps_configuration = {
+    cos_apikey               = local.cos_apikey
+    cos_region               = var.ibmcloud_cos_configuration.cos_region
+    cos_resource_instance_id = local.cos_resource_instance_id
+    cos_bucket_name          = var.ibmcloud_cos_configuration.cos_bucket_name
+    cos_dir_name             = var.ibmcloud_cos_configuration.cos_cps_software_path
+    download_dir_path        = "/tmp"
+  }
+}
+
+
+module "ibmcloud_cos_download_cps_binaries" {
+  #depends_on = module.powervs_infra
+
+  source  = "terraform-ibm-modules/powervs-sap/ibm//modules/ibmcloud-cos"
+  version = "3.0.0"
+
+
+  access_host_or_ip          = "150.240.163.42"
+  target_server_ip           = "10.30.20.5"
+  ssh_private_key            = var.ssh_private_key
+  ibmcloud_cos_configuration = local.ibmcloud_cos_cps_configuration
+}
+
+
+resource "terraform_data" "setup_cps_host" {
+
+  connection {
+    type         = "ssh"
+    user         = "root"
+    bastion_host = "150.240.163.42"
+    host         = "10.30.20.5"
+    private_key  = var.ssh_private_key
+    agent        = false
+    timeout      = "5m"
+  }
+
+  # Create installable  directory
+  provisioner "remote-exec" {
+    inline = ["mkdir -p /cps-sw-runtime/installable/",]
+  }
+
+}
+resource "terraform_data" "download_objects" {
+depends_on = [ module.ibmcloud_cos_download_cps_binaries ]
+  connection {
+    type         = "ssh"
+    user         = "root"
+    bastion_host = "150.240.163.42"
+    host         = "10.30.20.5"
+    private_key  = var.ssh_private_key
+    agent        = false
+    timeout      = "3m"
+  }
+
+# Untar installable
+  provisioner "remote-exec" {
+    inline = ["pwd","mv /tmp/CPSSW/ipas-software-2.3.5.0-20240912-1539.tgz/ipas-software-2.3.5.0-20240912-1539.tgz /cps-sw-runtime/installable/ipas-software-2.3.5.0-20240912-1539.tgz","cd /cps-sw-runtime/installable","tar -zxvf ipas-software-2.3.5.0-20240912-1539.tgz",]
+  }
+}
