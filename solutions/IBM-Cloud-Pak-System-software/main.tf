3a1d4bde7dd43a7a00cd0ae098afdaa525c6bb2d
Bincy <bincy.koshy@ibm.com>
2024-09-22 11:51:05 +0530

    feat : commit catalog SKIP UPGRADE TEST

--- solutions/IBM-Cloud-Pak-System-software/main.tf
+++ solutions/IBM-Cloud-Pak-System-software/main.tf
@@ -9,13 +9,12 @@ locals {
 }
 
 locals {
-    ibmcloud_cos_solution_configuration = {
+  ibmcloud_cos_solution_configuration = {
     cos_apikey               = local.cos_apikey
     cos_region               = var.ibmcloud_cos_configuration.cos_region
     cos_resource_instance_id = local.cos_resource_instance_id
     cos_bucket_name          = var.ibmcloud_cos_configuration.cos_bucket_name
-  #  cos_dir_name             = var.ibmcloud_cos_configuration.cos_solution_software_path
-    #download_dir_path        = local.network_services_config.nfs.nfs_client_path
+    cos_dir_name             = var.ibmcloud_cos_configuration.cos_solution_software_path
+    download_dir_path        = local.network_services_config.nfs.nfs_client_path
   }
 }
-
--- solutions/IBM-Cloud-Pak-System-software/variables.tf
+++ solutions/IBM-Cloud-Pak-System-software/variables.tf
@@ -4,10 +4,10 @@
 #
 ################################################################
 
-#variable "prerequisite_workspace_id" {
- # description = "IBM Cloud Schematics workspace ID of an existing 'Power Virtual Server with VPC landing zone' catalog solution. If you do not yet have an existing deployment, click [here](https://cloud.ibm.com/catalog/architecture/deploy-arch-ibm-pvs-inf-2dd486c7-b317-4aaa-907b-42671485ad96-global?) to create one."
-  #type        = string
-#}
+variable "prerequisite_workspace_id" {
+  description = "IBM Cloud Schematics workspace ID of an existing 'Power Virtual Server with VPC landing zone' catalog solution. If you do not yet have an existing deployment, click [here](https://cloud.ibm.com/catalog/architecture/deploy-arch-ibm-pvs-inf-2dd486c7-b317-4aaa-907b-42671485ad96-global?) to create one."
+  type        = string
+}
 
 variable "ibmcloud_cos_service_credentials" {
   description = "IBM Cloud Object Storage instance service credentials to access the bucket in the instance.[json example of service credential](https://cloud.ibm.com/docs/cloud-object-storage?topic=cloud-object-storage-service-credentials)"
@@ -18,15 +18,12 @@ variable "ibmcloud_cos_service_credentials" {
 variable "ibmcloud_cos_configuration" {
   description = "Cloud Object Storage instance containing SAP installation files that will be downloaded to NFS share. 'cos_hana_software_path' must contain only binaries required for HANA DB installation. 'cos_solution_software_path' must contain only binaries required for S/4HANA or BW/4HANA installation and must not contain any IMDB files. The binaries required for installation can be found [here](https://github.com/terraform-ibm-modules/terraform-ibm-powervs-sap/blob/main/solutions/ibm-catalog/sap-s4hana-bw4hana/docs/s4hana23_bw4hana21_binaries.md) If you have an optional stack xml file (maintenance planner), place it under the 'cos_solution_software_path' directory. Avoid inserting '/' at the beginning for 'cos_hana_software_path' and 'cos_solution_software_path'."
   type = object({
-    cos_region                  = string
-    cos_bucket_name             = string
+    cos_region      = string
+    cos_bucket_name = string
   })
   default = {
     "cos_region" : "eu-geo",
     "cos_bucket_name" : "powervs-automation",
-    "cos_hana_software_path" : "HANA_DB/rev78",
-    "cos_solution_software_path" : "S4HANA_2023",
-    "cos_swpm_mp_stack_file_name" : ""
   }
 }
 
@@ -54,4 +51,3 @@ variable "ibmcloud_api_key" {
 # Optional Parameters
 #
 ################################################################
-
