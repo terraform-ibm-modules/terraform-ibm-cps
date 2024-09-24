e59589c47b85dcd5d1f0671ec64ce588f3eae7b3
gayathribheemesh <gayathrib@in.ibm.com>
2024-09-24 11:17:03 +0530

    Update variables.tf

--- solutions/CPS_POWER/variables.tf
+++ solutions/CPS_POWER/variables.tf
@@ -1,14 +1,12 @@
+################################################################
+#
+# Required Parameters
+#
+################################################################
 
-variable "ssh_private_key" {
-  description = "Private SSH key (RSA format) to login to Intel VSIs to configure network management services (SQUID, NTP, NFS, DNS and ansible). Should match to public SSH key referenced by 'ssh_public_key'. The key is not uploaded or stored. For more information about SSH keys, see [SSH keys](https://cloud.ibm.com/docs/vpc?topic=vpc-ssh-keys)."
+variable "prerequisite_workspace_id" {
+  description = "IBM Cloud Schematics workspace ID of an existing 'Power Virtual Server with VPC landing zone' catalog solution. If you do not yet have an existing deployment, click [here](https://cloud.ibm.com/catalog/architecture/deploy-arch-ibm-pvs-inf-2dd486c7-b317-4aaa-907b-42671485ad96-global?) to create one."
   type        = string
-  sensitive   = true
-}
-
-variable "ibmcloud_api_key" {
-  description = "The IBM Cloud platform API key needed to deploy IAM enabled resources."
-  type        = string
-  sensitive   = true
 }
 
 variable "ibmcloud_cos_service_credentials" {
@@ -20,147 +18,32 @@ variable "ibmcloud_cos_service_credentials" {
 variable "ibmcloud_cos_configuration" {
   description = "Cloud Object Storage instance containing SAP installation files that will be downloaded to NFS share. 'cos_hana_software_path' must contain only binaries required for HANA DB installation. 'cos_solution_software_path' must contain only binaries required for S/4HANA or BW/4HANA installation and must not contain any IMDB files. The binaries required for installation can be found [here](https://github.com/terraform-ibm-modules/terraform-ibm-powervs-sap/blob/main/solutions/ibm-catalog/sap-s4hana-bw4hana/docs/s4hana23_bw4hana21_binaries.md) If you have an optional stack xml file (maintenance planner), place it under the 'cos_solution_software_path' directory. Avoid inserting '/' at the beginning for 'cos_hana_software_path' and 'cos_solution_software_path'."
   type = object({
-    cos_region                  = string
-    cos_bucket_name             = string
-    cos_cps_software_path      = string
+    cos_region      = string
+    cos_bucket_name = string
+    cos_solution_software_path = string
   })
-
-}
-#####################################################
-# Optional Parameters IBM Cloud Services
-#####################################################
-
-variable "transit_gateway_global" {
-  description = "Connect to the networks outside the associated region."
-  type        = string
-  default     = false
-}
-
-#####################################################
-# Optional Parameters VSI OS Management Services
-#####################################################
-
-variable "configure_dns_forwarder" {
-  description = "Specify if DNS forwarder will be configured. This will allow you to use central DNS servers (e.g. IBM Cloud DNS servers) sitting outside of the created IBM PowerVS infrastructure. If yes, ensure 'dns_forwarder_config' optional variable is set properly. DNS forwarder will be installed on the network-services vsi."
-  type        = bool
-  default     = true
-}
-
-variable "configure_ntp_forwarder" {
-  description = "Specify if NTP forwarder will be configured. This will allow you to synchronize time between IBM PowerVS instances. NTP forwarder will be installed on the network-services vsi."
-  type        = bool
-  default     = true
-}
-
-variable "configure_nfs_server" {
-  description = "Specify if NFS server will be configured. This will allow you easily to share files between PowerVS instances (e.g., SAP installation files). [File storage share and mount target](https://cloud.ibm.com/docs/vpc?topic=vpc-file-storage-create&interface=ui) in VPC will be created.. If yes, ensure 'nfs_server_config' optional variable is set properly below. Default value is '200GB' which will be mounted on specified directory in network-service vsi."
-  type        = bool
-  default     = true
-}
-
-variable "dns_forwarder_config" {
-  description = "Configuration for the DNS forwarder to a DNS service that is not reachable directly from PowerVS."
-  type = object({
-    dns_servers = string
-  })
-
   default = {
-    "dns_servers" : "161.26.0.7; 161.26.0.8; 9.9.9.9;"
+    "cos_region" : "us-south",
+    "cos_bucket_name" : "cps-test-1",
+    "cos_solution_software_path" : "TEST/common-utils.py",
+
   }
 }
 
-variable "nfs_server_config" {
-  description = "Configuration for the NFS server. 'size' is in GB, 'iops' is maximum input/output operation performance bandwidth per second, 'mount_path' defines the target mount point on os. Set 'configure_nfs_server' to false to ignore creating file storage share."
-  type = object({
-    size       = number
-    iops       = number
-    mount_path = string
-  })
-
-  default = {
-    "size" : 200,
-    "iops" : 600,
-    "mount_path" : "/nfs"
-  }
-}
-
-#####################################################
-# Optional Parameters PowerVS Workspace
-#####################################################
-
-variable "powervs_management_network" {
-  description = "Name of the IBM Cloud PowerVS management subnet and CIDR to create."
-  type = object({
-    name = string
-    cidr = string
-  })
-
-  default = {
-    "name" : "mgmt_net",
-    "cidr" : "10.51.0.0/24"
-  }
-}
-
-variable "powervs_backup_network" {
-  description = "Name of the IBM Cloud PowerVS backup network and CIDR to create."
-  type = object({
-    name = string
-    cidr = string
-  })
-
-  default = {
-    "name" : "bkp_net",
-    "cidr" : "10.52.0.0/24"
-  }
-}
-
-variable "powervs_image_names" {
-  description = "List of Images to be imported into cloud account from catalog images. Supported values can be found [here](https://github.com/terraform-ibm-modules/terraform-ibm-powervs-workspace/blob/main/docs/catalog_images_list.md)"
-  type        = list(string)
-  default     = ["IBMi-75-03-2924-2", "IBMi-74-09-2984-1", "7200-05-07", "7300-02-01", "SLES15-SP5-SAP", "SLES15-SP5-SAP-NETWEAVER", "RHEL9-SP2-SAP", "RHEL9-SP2-SAP-NETWEAVER"]
-}
-
-variable "tags" {
-  description = "List of tag names for the IBM Cloud PowerVS workspace"
-  type        = list(string)
-  default     = []
-}
-
-#####################################################
-# Optional Parameters Secret Manager
-#####################################################
-
-variable "sm_service_plan" {
+variable "software_download_directory" {
+  description = "Software installation binaries will be downloaded to this directory."
   type        = string
-  description = "The service/pricing plan to use when provisioning a new Secrets Manager instance. Allowed values: `standard` and `trial`. Only used if `existing_sm_instance_guid` is set to null."
-  default     = "standard"
+  default     = "/software"
 }
 
-variable "existing_sm_instance_guid" {
+variable "ssh_private_key" {
+  description = "Private SSH key (RSA format) used to login to IBM PowerVS instances. Should match to uploaded public SSH key referenced by 'ssh_public_key' which was created previously. The key is temporarily stored and deleted. For more information about SSH keys, see [SSH keys](https://cloud.ibm.com/docs/vpc?topic=vpc-ssh-keys)."
   type        = string
-  description = "An existing Secrets Manager GUID. The existing Secret Manager instance must have private certificate engine configured. If not provided an new instance will be provisioned."
-  default     = null
+  sensitive   = true
 }
 
-variable "existing_sm_instance_region" {
+variable "ibmcloud_api_key" {
+  description = "IBM Cloud platform API key needed to deploy IAM enabled resources."
   type        = string
-  description = "Required if value is passed into `var.existing_sm_instance_guid`."
-  default     = null
-}
-
-variable "certificate_template_name" {
-  type        = string
-  description = "The name of the Certificate Template to create for a private_cert secret engine. When `var.existing_sm_instance_guid` is not null, then it has to be the existing template name that exists in the private cert engine."
-  default     = "my-template"
-}
-
-#############################################################################
-# Schematics Output
-#############################################################################
-
-# tflint-ignore: all
-variable "IC_SCHEMATICS_WORKSPACE_ID" {
-  description = "leave blank if running locally. This variable will be automatically populated if running from an IBM Cloud Schematics workspace."
-  type        = string
-  default     = ""
+  sensitive   = true
 }
