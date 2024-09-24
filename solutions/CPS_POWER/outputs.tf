4de735ed9b41cb13ab48115467e5d1b9c607aa37
gayathribheemesh <gayathrib@in.ibm.com>
2024-09-24 11:18:10 +0530

    Create outputs.tf

---
+++ solutions/CPS_POWER/outputs.tf
@@ -0,0 +1,9 @@
+output "infrastructure_data" {
+  description = "PowerVS infrastructure details."
+  value       = { for k, v in local.powervs_infrastructure[0] : k => v.value }
+}
+
+output "access_host_or_ip" {
+  description = "Public IP of Provided Bastion/JumpServer Host."
+  value       = local.access_host_or_ip
+}
