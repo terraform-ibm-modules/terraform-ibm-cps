1482984554438cb78edc5f45092a4d4573e0c2fb
gayathribheemesh <gayathrib@in.ibm.com>
2024-09-26 20:27:01 +0530

    Update main.tf

--- solutions/CPS_POWER/main.tf
+++ solutions/CPS_POWER/main.tf
@@ -67,7 +67,7 @@ depends_on = [ module.ibmcloud_cos_download_cps_binaries ]
              "cd /cps-sw-runtime/installable/",
              "unzip CPSBINARIES.zip",
              "cd /cps-sw-runtime/installable/CPSBINARIES",
-             "tar -zxvf ipas-software-2.3.5.0-20240925-0620.tgz",
+             "tar -zxvf ipas-software-2.3.5.0-20240925-1752.tgz",
              "sh prereq.sh",
              #"sed -i 's/yum -y install $${IPAS_RPM}/yum -y install --nogpgcheck $${IPAS_RPM}/g' /cps-sw-runtime/installable/CPSBINARIES/install.sh",
              "sed -i 's/INSTALLATION_UNIQUE_NAME=/INSTALLATION_UNIQUE_NAME=cps2350/g' /cps-sw-runtime/installable/CPSBINARIES/samples/software_response_file",
